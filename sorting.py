1afac9b9533df4cfbfc0f9b93847657402f8ed50
CyberFlame <cyberflameu@gmail.com>
2022-02-11 15:04:19 +1300

    implement no-globals approach to swap_count

--- sorting.py
+++ sorting.py
@@ -4,20 +4,28 @@ Bubble sorting
 """
 
 
-def bubble_sort(arr: list):
+# swap_count: int = 0
+
+def bubble_sort(array: list) -> object:
     """
     Sorts an array (list) using bubble sorting
-    :param arr:
+    :param array:
+    :rtype: object
+    :return: tuple
     """
-    n = len(arr)
+    n = len(array)
+    sorted_array = array.copy()
 
+    swap_count = 0
     for i in range(0, n - 1):
         for j in range(n - i - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+            if sorted_array[j] > sorted_array[j + 1]:
+                sorted_array[j], sorted_array[j + 1] = sorted_array[j + 1], sorted_array[j]
+                swap_count += 1
+    return sorted_array, swap_count
 
 
-array_test = [1, 2, 6, 3, 2, 5, 6, 77, 3, 21, 4]
-bubble_sort(array_test)
+array_test = [1, 2, 6, 3, 2, 5, 6, 77, 3, 21, 4, 8]
 
-print(array_test)
+sorted_arr, num_swaps = bubble_sort(array_test)
+print("Unsorted list: ", array_test, "\nSorted list: ", sorted_arr, f"\nSwap count: {num_swaps}", sep = "")
