6b12dc4298fd39bcc67d0730dc9373054ff0e75f
CyberFlame <cyberflameu@gmail.com>
2022-02-24 10:11:50 +1300

    hhh

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
-﻿import pygame
+﻿"""
+SNEKSY GAME
+"""
+
+from typing import Union
+
+import pygame
 from random import randint
 
-FOOD_SIZE = 10
-DISPLAY_WIDTH = 640
-DISPLAY_HEIGHT = 480
-BG_COLOUR = (44, 62, 80)
-SNAKE_COLOUR = (236, 240, 241)
-FOOD_COLOUR = (241, 196, 15)
+from pygame import Rect, Surface
+from pygame.rect import RectType
+from pygame.surface import SurfaceType
+from pygame.time import Clock
+
+FOOD_SIZE: int = 10
+DISPLAY_WIDTH: int = 640
+DISPLAY_HEIGHT: int = 480
+BG_COLOR: tuple[int, int, int] = (44, 62, 80)
+SNAKE_COLOR: tuple[int, int, int] = (236, 240, 241)
+FOOD_COLOR: tuple[int, int, int] = (241, 196, 15)
 
 
-def draw_food(display, coords):
-    new_food_list = []
+def draw_food(display: object, coords: object) -> object:
+    """
+    Food drawer
+    :rtype: object
+    :param display:
+    :type coords: object
+
+    :return:
+    """
+    new_food_list: list[Union[Rect, RectType]] = []
+    pair: object
     for pair in coords:
         new_food_list.append(pygame.draw.rect(display,
-                                              FOOD_COLOUR,
+                                              FOOD_COLOR,
                                               (pair[0],
                                                pair[1],
                                                FOOD_SIZE, FOOD_SIZE)))
@@ -21,40 +41,47 @@ def draw_food(display, coords):
 
 
 def new_food():
-    new_x = randint(0, DISPLAY_WIDTH - 1) // FOOD_SIZE * FOOD_SIZE
-    new_y = randint(0, DISPLAY_HEIGHT - 1) // FOOD_SIZE * FOOD_SIZE
+    """
+    Food gen
+    :return:
+    """
+    new_x: int = randint(0, DISPLAY_WIDTH - 1) // FOOD_SIZE * FOOD_SIZE
+    new_y: int = randint(0, DISPLAY_HEIGHT - 1) // FOOD_SIZE * FOOD_SIZE
     return [new_x, new_y]
 
 
 def main():
     pygame.init()
     pygame.display.set_caption("Snakesy")
-    clock = pygame.time.Clock()
-    snake_display = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+    clock: Clock = pygame.time.Clock()
+    snake_display: Union[Surface, SurfaceType] = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 
     pygame.display.update()
 
     # generate food
-    food_coordinates = []
+    food_coordinates: list[list[int]] = []
     # for i in range(4):
     #     new_x = randint(0, DISPLAY_WIDTH - 1) // FOOD_SIZE * FOOD_SIZE
     #     new_y = randint(0, DISPLAY_HEIGHT - 1) // FOOD_SIZE * FOOD_SIZE
     #     food_coordinates.append([new_x, new_y])
     # print(food_coordinates)
 
-    snake_x = DISPLAY_WIDTH / 2
-    snake_y = DISPLAY_HEIGHT / 2
-    snake_rectangle = pygame.Rect(snake_x, snake_y, FOOD_SIZE, FOOD_SIZE)
-    snake_x_change = FOOD_SIZE
-    snake_y_change = 0
+    score: int = 0
+
+    snake_x: float = DISPLAY_WIDTH / 2
+    snake_y: float = DISPLAY_HEIGHT / 2
+    snake_rectangle: Rect = pygame.Rect(snake_x, snake_y, FOOD_SIZE, FOOD_SIZE)
+    snake_x_change: int = FOOD_SIZE
+    snake_y_change: int = 0
     game_over = False
     while not game_over:
         if len(food_coordinates) == 0:
             food_coordinates.append(new_food())
         clock.tick(25)
-        snake_display.fill(BG_COLOUR)
-        snake = pygame.draw.rect(snake_display, SNAKE_COLOUR, snake_rectangle)
-        food_list = draw_food(snake_display, food_coordinates)
+        snake_display.fill(BG_COLOR)
+
+        snake: Union[Rect, RectType] = pygame.draw.rect(snake_display, SNAKE_COLOR, snake_rectangle)
+        food_list: list[Union[Rect, RectType]] = draw_food(snake_display, food_coordinates)
         pygame.display.update()
         for event in pygame.event.get():
             match event.type:
@@ -76,7 +103,7 @@ def main():
                             snake_y_change = FOOD_SIZE
 
 
-        snake_rectangle = snake_rectangle.move(snake_x_change, snake_y_change)
+        snake_rectangle: object = snake_rectangle.move(snake_x_change, snake_y_change)
         if snake.x < 0:
             snake_rectangle.update(DISPLAY_WIDTH-FOOD_SIZE, snake.y, FOOD_SIZE, FOOD_SIZE)
         elif snake.x > DISPLAY_WIDTH-FOOD_SIZE:
@@ -88,6 +115,8 @@ def main():
 
         if snake.collidelist(food_list) >= 0:
             food_coordinates.pop(snake.collidelist(food_list))
+            score += 1
+            print(score)
 
 
 if __name__ == "__main__":
