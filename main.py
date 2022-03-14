a628e5c91a198ba856615a59cfde71d4d756a746
Aaron Lowe <cyberflameu@gmail.com>
2022-03-14 15:17:55 +1300

    no idea what just happened

--- main.py
+++ main.py
@@ -5,46 +5,68 @@ Work for when we are away
 from typing import Union
 
 import os
+
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
 
 import pygame  # noqa: E402
 
 DISPLAY_WIDTH: int = 500
 DISPLAY_HEIGHT: int = 500
-WHITE = (255, 255, 255)
+WHITE: tuple[int, int, int] = (255, 255, 255)
+BLACK: tuple[int, int, int] = (0, 0, 0)
+DEEP_RED: tuple[int, int, int] = (255, 0, 0)
 
 pygame.init()
 
 display_surface: Union[pygame.Surface, pygame.SurfaceType] = pygame.display.set_mode(
     (DISPLAY_WIDTH, DISPLAY_HEIGHT)
 )
-pygame.display.set_caption("dn")
-x: int = 0
-y: int = 0
-
-width: int = 40
-height: int = 60
-velocity: int = 5
 
 
 def main():
     """
     Main function
     """
+    x: int = 0
+    y: int = 0
+    width: int = 40
+    height: int = 60
+    velocity: int = 5
+    jumps: int = 0
+    jumping: bool = False
     pygame.display.set_caption("dn")
     playing: bool = True
     while playing:
         for event in pygame.event.get():
-            match event.type:
-                case pygame.QUIT:
-                    playing = False
-                    continue
-                case pygame.MOUSEBUTTONDOWN:
-                    # Check if left click
-                    if pygame.mouse.get_pressed() == (1, 0, 0):
-                        x, y = pygame.mouse.get_pos()
-                        if image.get_rect().collidepoint(x, y):
-                            print(u"printing dn")
+            if event.type == pygame.QUIT:
+                playing = False
+            elif event.type == pygame.KEYDOWN:
+                match event.key:
+                    case pygame.K_LEFT:
+                        if x > velocity:
+                            x -= velocity
+                    case pygame.K_RIGHT:
+                        if x < DISPLAY_WIDTH - width - velocity:
+                            x += velocity
+                    case pygame.K_UP:
+                        if y > velocity and not jumping:
+                            y -= velocity
+                    case pygame.K_DOWN:
+                        if y < DISPLAY_HEIGHT - height - velocity and not jumping:
+                            y += velocity
+                    case pygame.K_SPACE:
+                        if not jumping:
+                            jumping = True
+            else:
+                while jumping:
+                    if jumps >= -10:
+                        y -= (jumps * abs(jumps)) * 0.5
+                        jumps -= 1
+                    else:
+                        jumps = 10
+                        jumping = False
+                display_surface.fill(BLACK)
+            red_rectangle = pygame.draw.rect(display_surface, DEEP_RED, (x, y, width, height))
             pygame.display.update()
 
 
