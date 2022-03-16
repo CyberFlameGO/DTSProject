f0965684f46064767c443d2425d567fcdcea7981
CyberFlame <cyberflameu@gmail.com>
2022-03-17 09:53:51 +1300

    refactor everything to the module level for now

--- main.py
+++ main.py
@@ -10,16 +10,37 @@ os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
 
 import pygame  # noqa: E402 isort: skip
 
-DISPLAY_WIDTH: int = 1000
-DISPLAY_HEIGHT: int = 1000
+DISPLAY_WIDTH: int = 500
+DISPLAY_HEIGHT: int = 500
 WHITE: tuple[int, int, int] = (255, 255, 255)
 BLACK: tuple[int, int, int] = (0, 0, 0)
 DEEP_RED: tuple[int, int, int] = (255, 0, 0)
 
 pygame.init()
 
+x: int = 0
+y: int = 400
+width: int = 64
+height: int = 64
+velocity: int = 5
+jumps: int = 10
+jumping: bool = False
+run: bool = True
+left: bool = False
+right: bool = False
 walk_count: int = 0
+walk_right: list = [pygame.image.load(r'./assets/R1.png'), pygame.image.load(r'./assets/R2.png'),
+                    pygame.image.load(r'./assets/R3.png'), pygame.image.load(r'./assets/R4.png'),
+                    pygame.image.load(r'./assets/R5.png'), pygame.image.load(r'./assets/R6.png'),
+                    pygame.image.load(r'./assets/R7.png'), pygame.image.load(r'./assets/R8.png'),
+                    pygame.image.load(r'./assets/R9.png')]
+walk_left: list = [pygame.image.load(r'./assets/L1.png'), pygame.image.load(r'./assets/L2.png'),
+                   pygame.image.load(r'./assets/L3.png'), pygame.image.load(r'./assets/L4.png'),
+                   pygame.image.load(r'./assets/L5.png'), pygame.image.load(r'./assets/L6.png'),
+                   pygame.image.load(r'./assets/L7.png'), pygame.image.load(r'./assets/L8.png'),
+                   pygame.image.load(r'./assets/L9.png')]
 bg = pygame.image.load(r'./assets/bg.jpg')
+char = pygame.image.load(r'./assets/standing.png')
 
 display_surface: Union[pygame.Surface, pygame.SurfaceType] = pygame.display.set_mode(
     (DISPLAY_WIDTH, DISPLAY_HEIGHT)
@@ -39,29 +60,7 @@ def main():
     """
     Main function
     """
-    x: int = 0
-    y: int = 400
-    width: int = 64
-    height: int = 64
-    velocity: int = 5
-    jumps: int = 10
-    jumping: bool = False
-    run: bool = True
-    left: bool = False
-    right: bool = False
     pygame.display.set_caption("dn")
-    walk_right: list = [pygame.image.load(r'./assets/R1.png'), pygame.image.load(r'./assets/R2.png'),
-                        pygame.image.load(r'./assets/R3.png'), pygame.image.load(r'./assets/R4.png'),
-                        pygame.image.load(r'./assets/R5.png'), pygame.image.load(r'./assets/R6.png'),
-                        pygame.image.load(r'./assets/R7.png'), pygame.image.load(r'./assets/R8.png'),
-                        pygame.image.load(r'./assets/R9.png')]
-    walk_left: list = [pygame.image.load(r'./assets/L1.png'), pygame.image.load(r'./assets/L2.png'),
-                       pygame.image.load(r'./assets/L3.png'), pygame.image.load(r'./assets/L4.png'),
-                       pygame.image.load(r'./assets/L5.png'), pygame.image.load(r'./assets/L6.png'),
-                       pygame.image.load(r'./assets/L7.png'), pygame.image.load(r'./assets/L8.png'),
-                       pygame.image.load(r'./assets/L9.png')]
-    bg = pygame.image.load(r'./assets/bg.jpg')
-    char = pygame.image.load(r'./assets/standing.png')
     playing: bool = True
     while playing:
         for event in pygame.event.get():
